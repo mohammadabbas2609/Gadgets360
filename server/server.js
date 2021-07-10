7982d93a92b1f9e8c5170e671f5c5a5897de090c
abbas <shaikh.abbas2609@gmail.com>
2021-07-10 16:08:03 +0530

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -29,6 +29,14 @@ app.get("/api/config/razorpay", (req, res) => {
 });
 
 const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
